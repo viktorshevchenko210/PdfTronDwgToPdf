0def2c8854a1b1e4357962a2b8011248aae2e5b4
Viktor Shevchenko <viktor.shevchenko@idealscorp.com>
2023-01-29 15:22:31 +0200

    First commit

---
+++ PdfTronDwgToPdf/Program.cs
@@ -0,0 +1,26 @@
+﻿using pdftron;
+
+string inputDirectory = Path.Combine(Directory.GetCurrentDirectory(), "in");
+string outputDirectory = Path.Combine(Directory.GetCurrentDirectory(), "out");
+if (!Directory.Exists(outputDirectory))
+    Directory.CreateDirectory(outputDirectory);
+
+foreach (var file in Directory.GetFiles(outputDirectory))
+    File.Delete(file);
+
+var inputFile = Directory.GetFiles(inputDirectory)[0];
+
+
+PDFNet.Initialize("demo:1638727123163:7b63556e0300000000e733b5223644478c184cc841cc180fb5c16d6939");
+PDFNet.AddResourceSearchPath(Path.Combine(Directory.GetCurrentDirectory(), "CADModuleLinux/Lib"));
+
+using (var doc = new pdftron.PDF.PDFDoc())
+{
+    var options = new pdftron.PDF.CADConvertOptions();
+    options.SetAllowThinLines(false);
+    options.SetPageWidth(800);
+    options.SetPageHeight(600);
+
+    pdftron.PDF.Convert.FromCAD(doc, inputFile, options);
+    doc.Save(Path.Combine(outputDirectory, $"PDFTron_{Guid.NewGuid()}.pdf"), pdftron.SDF.SDFDoc.SaveOptions.e_linearized);
+}
